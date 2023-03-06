24c0c6bba404f3ac3403ca948a169b7d98a9ec13
Jun Eeo <jun.eeo@thehutgroup.com>
2023-03-06 12:10:39 +0000

    fix script

--- update-charts.sh
+++ update-charts.sh
@@ -3,8 +3,9 @@ set -x
 
 #CLOUD_PROVIDER_VERSION=master CHART_FOLDER=helm/cloud-provider-openstack-app/helm/cloud-provider-openstack-app/ ./script/update-cloud-provider-app.sh
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 export version=${CLOUD_PROVIDER_VERSION?}
-export current_branch=$(git rev-parse --abbrev-ref HEAD)
+export current_branch="$current_branch"
 
 git remote add -f upstream-copy git@github.com:kubernetes/cloud-provider-openstack.git 
 
@@ -13,13 +14,13 @@ git remote add -f upstream-copy git@github.com:kubernetes/cloud-provider-opensta
 #   helm/cloud-provider-openstack-app/charts/openstack-cinder-csi
 set -e
 git fetch upstream-copy
-git checkout ${version}
+git checkout "${version}"
 git subtree split -P charts/cinder-csi-plugin/ -b temp-split-branch
-git checkout ${current_branch}
-if [$1 -eq '--add']; then
-    git subtree add --squash -P ${CHART_FOLDER?}charts/openstack-cinder-csi temp-split-branch
+git checkout "${current_branch}"
+if [ "$1" = '--add' ]; then
+    git subtree add --squash -P "${CHART_FOLDER?}charts/openstack-cinder-csi" temp-split-branch
 else
-    git subtree merge --squash -P ${CHART_FOLDER?}charts/openstack-cinder-csi temp-split-branch
+    git subtree merge --squash -P "${CHART_FOLDER?}charts/openstack-cinder-csi" temp-split-branch
 fi
 
 git branch -D temp-split-branch
@@ -30,20 +31,20 @@ git branch -D temp-split-branch
 #  helm/cloud-provider-openstack-app/charts/openstack-cloud-controller-manager
 
 git fetch upstream-copy
-git checkout ${version}
+git checkout "${version}"
 git subtree split -P charts/openstack-cloud-controller-manager -b temp-split-branch
-git checkout ${current_branch?}
+git checkout "${current_branch?}"
 
 
-if [$1 -eq '--add']; then
-    git subtree add --squash -P ${CHART_FOLDER?}charts/openstack-cloud-controller-manager temp-split-branch
+if [ "$1" = '--add' ]; then
+    git subtree add --squash -P "${CHART_FOLDER?}charts/openstack-cloud-controller-manager" temp-split-branch
 else
-    git subtree merge --squash -P ${CHART_FOLDER?}charts/openstack-cloud-controller-manager temp-split-branch
+    git subtree merge --squash -P "${CHART_FOLDER?}charts/openstack-cloud-controller-manager" temp-split-branch
 
 fi
 
 git branch -D temp-split-branch
 
 
-# remove remote 
+# remove remote
 git remote remove upstream-copy
